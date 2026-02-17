7a1a62638b3f273c14c14d6a17ab4f5fc11373f7
Collen Malvern Madembo <57815345+Malvern97@users.noreply.github.com>
2026-02-17 11:28:05 +0200

    Refactor login form and session handling
    Refactored login handling and improved user input handling.

--- index.php
+++ index.php
@@ -1,24 +1,69 @@
 <?php
 session_start();
+require(connect.php);
+/*User Variable Initialization*/
+$name= "$_POST['name']";
+$password= "$_POST['password']";
+
+/*Admin Login Database Query*/
+$sql= " SELECT* FROM admin ";
+$query= mysqli_query( $conn, $sql);
+
+/*Guards Login Database Query*/
+$sql= " SELECT* FROM guard ";
+$query2= mysqli_query( $conn, $sql);
+
+
+/*Admin Login Handler*/
+if(isset($_POST['submit'])){
+    while($row= mysqli_fetch_assoc($query) ){
+        /*User Authentication*/
+        if($name==$row['name'] && $password=$row['password'] ){
+            
+            $_SESSION['msg']= echo"You successfully logged in";
+            header('location:adminhome.php');
+        }
+        else{
+            echo" Incorrect Credentials";
+            exit();
+        }
+    }
+}
+/*Guard Login Handler*/
+elseif(isset($_POST['submit'])){
+    while($row= mysqli_fetch_assoc($query2) ){
+    /*User Authentication*/
+    if($name==$row['name'] && $password=$row['password'] ){
+        
+        $_SESSION['msg']= echo"You successfully logged in";
+        header('location:homepage.php');
+    }
+    else{
+        echo" Incorrect Credentials";
+        exit();
+    }
+}
+}
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lodge Visitors</title>
+    <title>Document</title>
+    <link rel='stylesheet' >
 </head>
 <body>
-    <div class='loginform'>
-        <input type="text" name='name' placeholder='Pleas enter your name' class='inputfield'><br>
-        <input type="password" name='password' class='inputfield' placeholder='Pleas enter your password'><br>
-        <button name='submit' class='loginbtn'> Login </button><br>
-        <a href='registratioin.php'><p>Create an account</p></a>
-        
-    </div>
+    <!--The User Login Form-->
+    <form method='post' action='index.php'>
+        <div class='loginform'>
+            <input type="text" name='name' placeholder='Please enter your name'>
+            <input type="password" name='password' placeholder='Please enter your password'>
+            <button name='submit' class='loginbtn'> Login </button><br>
+            <a href='registratioin.php'><p>Create an account</p></a>
+        </div>
+    </form>
 </body>
-
 </html>
-
-
-
