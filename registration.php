16f7e0b2076358394df17ac2ed1aa3ed07ca246a
Collen Malvern Madembo <57815345+Malvern97@users.noreply.github.com>
2026-02-17 11:44:22 +0200

    Update registration form to include phone and id fields

--- registration.php
+++ registration.php
@@ -4,6 +4,8 @@ require(connect.php);
 /*User Variable Initialization*/
 $name= "$_POST['name']";
 $email= "$_POST['email']";
+$phone= "$_POST['phone']";
+$id= "$_POST['id']";
 $password= "$_POST['password']";
 
 /*Admin Login Database Query*/
@@ -16,8 +18,11 @@ $query= mysqli_query( $conn, $sql);
 /*Admin Login Handler*/
 if(isset($_POST['submit'])){
     while($row= mysqli_fetch_assoc($query) ){
-    if($name != $row['name'] && $password != $row['password']){
-        $regsql= "INSERT INTO admin (name,email,password) VALUES('$name,$email,$password)";
+
+     /*Registration existing user checking and verification*/   
+    if($id != $row['id'] && $phone != $row['phone']){
+        /*Add a new Visitor*/
+        $regsql= "INSERT INTO admin (id,name,phone, email,password) VALUES('$id,$name,$phone, $email,$password)";
         $_SESSION['msg']= echo"You registered an account successfully";
         header('location:index.php')
     }
@@ -42,6 +47,9 @@ if(isset($_POST['submit'])){
     <form method='post' action='registration.php'>
         <div class='loginform'>
             <input type="text" name='name' placeholder='Please enter your name' required>
+            <input type="text" name='phone' placeholder='Please enter your phone' required>
+            <input type="text" name='id' placeholder='Please enter your id' required>
+
             <input type="email" name='email' placeholder='Please enter your email' required>
             <input type="password" name='password' placeholder='Please enter your password' required>
             <button name='submit' class='loginbtn'> Register </button><br>
